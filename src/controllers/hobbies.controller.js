8957f37fd459b48e90934cc10566cffa6aa8c31c
Zack Ankner <zackankner@gmail.com>
2020-05-03 17:32:32 -0400

    f

--- src/controllers/hobbies.controller.js
+++ src/controllers/hobbies.controller.js
@@ -27,6 +27,8 @@ module.exports.get = async (req, res) => {
           if (specific === 'musical') {
             hobby.name = 'Music Production';
             hobby.courses = hobbies.musicProduction;
+          }
+          if (specific === 'notMusical') {
             hobby.name = 'Graphic Design';
             hobby.courses = hobbies.graphicDesign;
           } else {
